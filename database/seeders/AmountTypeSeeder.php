8f82cb2855b229edbe4914d88af2553266fa81d6
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2024-12-28 12:00:33 +0500

    add AmountType model, seeder, migration

---
+++ database/seeders/AmountTypeSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\AmountType;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AmountTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        AmountType::create(['name' => 'qop']);
+        AmountType::create(['name' => 'tonna']);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            // Important
             RoleSeeder::class,
             UserSeeder::class,
+            AmountTypeSeeder::class,
+
+            // Optional
         ]);
     }
 }
