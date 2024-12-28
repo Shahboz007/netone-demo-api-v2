e74111194700562d7afbd55ebcace79f0c139fe5
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2024-12-28 11:46:29 +0500

    add role and role user migration and one relationship role with user

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -47,5 +48,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->roles()->where('name', 'admin')->exists();
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,29 +15,41 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
+        $admin = User::create([
             "name" => "Admin",
             "login" => "admin",
             "phone" => "998123456789",
             "password" => Hash::make("secret"),
         ]);
-        User::create([
+        $adminRole = Role::where('name', 'admin')->firstOrFail();
+        $admin->roles()->attach($adminRole);
+        
+        $storekeeper = User::create([
             "name" => "Storekeeper",
             "login" => "storekeeper",
             "phone" => "998123456788",
             "password" => Hash::make("secret"),
         ]);
-        User::create([
+        $storekeeperRole = Role::where('name', 'storekeeper')->firstOrFail();
+        $storekeeper->roles()->attach($storekeeperRole);
+
+        $producer = User::create([
             "name" => "Producer",
             "login" => "producer",
             "phone" => "998123456787",
             "password" => Hash::make("secret"),
         ]);
-        User::create([
+        $producerRole = Role::where('name', 'producer')->firstOrFail();
+        $producer->roles()->attach($producerRole);
+        
+        $orderer = User::create([
             "name" => "Orderer",
             "login" => "orderer",
             "phone" => "998123456786",
             "password" => Hash::make("secret"),
         ]);
+
+        $ordererRole = Role::where('name', 'orderer')->firstOrFail();
+        $orderer->roles()->attach($ordererRole);
     }
 }
