cd9f3a783f943a45619d1bc9f902e40d12f5670a
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2024-12-29 12:50:52 +0500

    Add ProductStock model, migration and relationship

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Product extends Model
 {
@@ -15,4 +16,9 @@ class Product extends Model
         'cost_price',
         'sale_price',
     ];
+
+    public function stock(): HasOne
+    {
+        return $this->hasOne(ProductStock::class, 'product_id');
+    }
 }
---
+++ database/migrations/2024_12_29_074302_create_product_stocks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_stocks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products')->cascadeOnDelete();
+            $table->string('name')->unique();
+            $table->foreignId('amount_type_id')->constrained('amount_types')->cascadeOnDelete();
+            $table->decimal('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_stocks');
+    }
+};
