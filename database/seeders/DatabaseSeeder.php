168f96d1dc678af5e60bf805525bc8ca687dd0f5
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2024-12-30 15:04:25 +0500

    added expense seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             ProductSeeder::class,
             CustomerSeeder::class,
             ProductStockSeeder::class,
+            ExpenseSeeder::class,
         ]);
     }
 }
