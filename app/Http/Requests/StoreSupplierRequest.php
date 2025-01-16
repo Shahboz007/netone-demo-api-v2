6360a45dfdd87eb695945207727d6d8b9f496978
shahboz007 <olmasovshahboz007@gmail.com>
2025-01-16 17:55:09 +0500

    Add supplier --all

---
+++ app/Http/Requests/StoreSupplierRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreSupplierRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'phone' => 'required|numeric|unique:customers,phone',
+            'telegram' => 'nullable|string|max:255|unique:customers,telegram',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'phone.unique' => 'Bu telefon raqam allaqachon mavjud!',
+            'telegram.unique' => 'Bu telegram ID allaqachon mavjud!'
+        ];
+    }
+}
