a1c0d8c460c0cd4641e4ea11e1cf22df871a7831
shahboz007 <olmasovshahboz007@gmail.com>
2025-01-17 08:35:36 +0500

    not finish production process

---
+++ app/Http/Controllers/ProductionProcessController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreProductionProcessRequest;
+use App\Http\Requests\UpdateProductionProcessRequest;
+use App\Models\ProductionProcess;
+use App\Models\Status;
+
+class ProductionProcessController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+
+    public function store(StoreProductionProcessRequest $request)
+    {
+        //
+    }
+
+
+    public function show(ProductionProcess $productionProcess)
+    {
+        //
+    }
+
+
+    public function update(UpdateProductionProcessRequest $request, ProductionProcess $productionProcess)
+    {
+        //
+    }
+
+
+    public function destroy(ProductionProcess $productionProcess)
+    {
+        //
+    }
+}
