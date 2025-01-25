099eab211263a241b71428e0c21c5924c95da2f2
shahboz007 <olmasovshahboz007@gmail.com>
2025-01-25 20:51:07 +0500

    process problem fixed

--- routes/api/production/process.php
+++ routes/api/production/process.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'production-processes', 'middleware' => ['auth:sanctum']], function(){
     Route::get('', [ProductionProcessController::class,'index']);
     Route::get('{id}', [ProductionProcessController::class,'show']);
+    Route::post('/', [ProductionProcessController::class,'store']);
     Route::put('{id}/finish', [ProductionProcessController::class,'finish']);
     Route::delete('{id}/cancel', [ProductionProcessController::class,'cancel']);
 });
