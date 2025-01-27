f9e1702a889429299826288156e731311aa2b5a1
shahboz007 <olmasovshahboz007@gmail.com>
2025-01-27 19:14:05 +0500

    change of order completed resource

--- app/Http/Resources/OrderCompletedResource.php
+++ app/Http/Resources/OrderCompletedResource.php
@@ -14,11 +14,13 @@ class OrderCompletedResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $oldBalance = (float)$this->customer_old_balance;
+
         return [
             'id' => $this->id,
-            'total_sale_price' => (float) $this->total_sale_price,
-            'total_const_price' => auth()->user()->isAdmin() ? (float) $this->total_cost_price : 0,
-            'customer_old_balance' => (float) $this->customer_old_balance,
+            'total_sale_price' => (float)$this->total_sale_price,
+            'total_const_price' => auth()->user()->isAdmin() ? (float)$this->total_cost_price : 0,
+            'customer_old_balance' => $oldBalance < 0 ? $oldBalance : 0,
             'comment' => $this->comment,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
