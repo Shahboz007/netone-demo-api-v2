43d378f04e77bed681c80ccb28e9dd7873ae4ab9
shahboz007 <olmasovshahboz007@gmail.com>
2025-01-28 10:14:39 +0500

    add current debt to OrderCompletedResource.php

--- app/Http/Resources/OrderCompletedResource.php
+++ app/Http/Resources/OrderCompletedResource.php
@@ -15,12 +15,16 @@ class OrderCompletedResource extends JsonResource
     public function toArray(Request $request): array
     {
         $oldBalance = (float)$this->customer_old_balance;
+        $oldBalance = min($oldBalance, 0);
+
+        $totalSalePrice = (float) $this->total_sale_price;
 
         return [
             'id' => $this->id,
-            'total_sale_price' => (float)$this->total_sale_price,
+            'total_sale_price' => $totalSalePrice,
             'total_const_price' => auth()->user()->isAdmin() ? (float)$this->total_cost_price : 0,
-            'customer_old_balance' => $oldBalance < 0 ? $oldBalance : 0,
+            'customer_old_balance' => $oldBalance,
+            'current_debt' => -$totalSalePrice + $oldBalance,
             'comment' => $this->comment,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
