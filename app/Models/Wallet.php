31869cc18bbd1a85663eae697b89c6b62b2ff841
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2025-01-29 16:43:39 +0500

    Add payment model with relationship

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -5,12 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Wallet extends Model
 {
-    /** @use HasFactory<\Database\Factories\WalletFactory> */
-    use HasFactory;
-
     protected $fillable = [
         'currency_id',
         'name',
@@ -20,5 +18,10 @@ class Wallet extends Model
     public function currency(): BelongsTo
     {
         return $this->belongsTo(Currency::class, 'currency_id');
+    }
+
+    public function payments():BelongsToMany
+    {
+        return $this->belongsToMany(Payment::class);
     }
 }
