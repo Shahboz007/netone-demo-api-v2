493031d290fd77a84ead7c00d8d1e52db644bc90
shahboz007 <olmasovshahboz007@gmail.com>
2025-02-01 18:12:25 +0500

    change of UserWallet

--- app/Http/Resources/UserWalletResource.php
+++ app/Http/Resources/UserWalletResource.php
@@ -19,6 +19,8 @@ class UserWalletResource extends JsonResource
             'user' => $this->user,
             'wallet' => WalletResource::make($this->wallet),
             'amount' => (float) $this->amount,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
         ];
     }
 }
