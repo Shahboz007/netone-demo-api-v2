ee0d4d74bc66aff1cd3aab3eefbd580ca9e423bc
shahboz007 <olmasovshahboz007@gmail.com>
2025-02-12 16:10:15 +0500

    done OrderReturnController store logic

--- app/Http/Controllers/OrderReturnController.php
+++ app/Http/Controllers/OrderReturnController.php
@@ -3,18 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreOrderReturnRequest;
-use App\Http\Requests\UpdateOrderReturnRequest;
 use App\Http\Resources\OrderReturnResource;
 use App\Http\Resources\OrderReturnShowResource;
+use App\Models\AmountType;
 use App\Models\Customer;
-use App\Models\Order;
-use App\Models\OrderDetail;
 use App\Models\OrderReturn;
+use App\Models\Product;
 use App\Models\ProductStock;
-use App\Models\Status;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
-use function PHPUnit\Framework\assertDirectoryDoesNotExist;
 
 class OrderReturnController extends Controller
 {
@@ -38,7 +35,119 @@ class OrderReturnController extends Controller
 
     public function store(StoreOrderReturnRequest $request)
     {
-        //
+        // Products
+        $products = Product::whereIn('id', array_column($request->validated('product_list'), 'product_id'))->get();
+        $pluckProductName = $products->pluck('name', 'id');
+        $pluckProductsSalePrice = $products->pluck('sale_price', 'id');
+        $pluckProductsCostPrice = $products->pluck('sale_price', 'id');
+
+        // Stock
+        $stock = ProductStock::whereIn('id', array_column($request->validated('product_list'), 'polka_id'))->get();
+        $pluckStock = $stock->pluck('product_id', 'id');
+        $pluckStockName = $stock->pluck('name', 'id');
+        $pluckStockAmounType = $stock->pluck('amount_type_id', 'id');
+
+        // Amount Types
+        $amountType = AmountType::whereIn('id', array_column($request->validated('product_list'), 'amount_type_id'))->get();
+        $pluckAmontTypeName = $amountType->pluck('name', 'id');
+
+        foreach ($request->validated('product_list') as $item) {
+
+            if (isset($pluckStock[$item['polka_id']])) {
+                // Check match
+                if ($pluckStock[$item['polka_id']] !== $item['product_id']) {
+
+
+                    $stockName = $pluckStockName[$item['polka_id']];
+                    $productName = $pluckProductName[$item['product_id']];
+
+                    return $this->mainErrRes("`$productName` mahsulot uchun `$stockName` nomli polka tegishli emas. Polkani to'g'ri tanlang");
+                }
+
+                // Check match amoun type with stock
+                if ($pluckStockAmounType[$item['polka_id']] !== $item['amount_type_id']) {
+                    $amountTypeName = $pluckAmontTypeName[$item['amount_type_id']];
+                    $polkaName = $pluckStockName[$item['polka_id']];
+                    $correctPolkaName = $pluckAmontTypeName[$pluckStockAmounType[$item['polka_id']]];
+
+                    return $this->mainErrRes("`$amountTypeName` o'lchov biriligi, `$polkaName` nomli polka uchun mos emas. `$polkaName` nomli polka faqat `$correctPolkaName` o'lchov biriligini qabul qiladi");
+                }
+            } else {
+                return $this->mainErrRes("Polka topilmadi");
+            }
+        }
+
+        // Customer
+        $customer = Customer::findOrFail($request->validated('customer_id'));
+
+
+        DB::beginTransaction();
+        try {
+            // Create New Return Order
+            $newReturnOrder = OrderReturn::create([
+                'user_id' => auth()->id(),
+                'customer_id' => $request->validated('customer_id'),
+                'total_sale_price' => 0,
+                'total_cost_price' => 0,
+            ]);
+
+            $totalSalePrice = 0;
+            $totalCostPrice = 0;
+
+
+            // Create Many Details Item
+            $list = [];
+
+            foreach ($request->validated('product_list') as $item) {
+                $costPrice = $pluckProductsCostPrice[$item['product_id']];
+                $salePrice = $pluckProductsSalePrice[$item['product_id']];
+
+                $sumCostPrice = $costPrice * $item['amount'];
+                $sumSalePrice = $salePrice * $item['amount'];
+
+                $list[] = [
+                    'order_return_id' => $newReturnOrder->id,
+                    'polka_id' => $item['polka_id'],
+                    'amount_type_id' => $item['amount_type_id'],
+                    'amount' => $item['amount'],
+                    'cost_price' => $costPrice,
+                    'sale_price' => $salePrice,
+                    'sum_cost_price' => $sumCostPrice,
+                    'sum_sale_price' => $sumSalePrice,
+                ];
+
+                $totalCostPrice += $sumCostPrice;
+                $totalSalePrice += $sumSalePrice;
+
+                // Change Stock
+                DB::table('product_stocks')
+                    ->where('id', $item['polka_id'])
+                    ->increment('amount', $item['amount']);
+            };
+
+            $newReturnOrder->orderReturnDetails()->createMany($list);
+
+            // Change Totals
+            $newReturnOrder->total_cost_price = $totalCostPrice;
+            $newReturnOrder->total_sale_price = $totalSalePrice;
+
+            $newReturnOrder->save();
+
+            // Change Customer Balance
+            $customer->increment('balance', $totalSalePrice);
+
+            // Finish
+            DB::commit();
+
+            $formatVal = number_format($totalSalePrice, 2, '.', ',');
+
+            return response()->json([
+                "message" => "Qaytarilgan yuk muvaffaqiyatli qabul qilindi. Jami summa $formatVal uzs"
+            ]);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return $this->serverError($e);
+        }
     }
 
 
@@ -55,6 +164,4 @@ class OrderReturnController extends Controller
             "data" => OrderReturnShowResource::make($data),
         ]);
     }
-
-
 }
--- app/Http/Requests/StoreOrderReturnRequest.php
+++ app/Http/Requests/StoreOrderReturnRequest.php
@@ -6,22 +6,17 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreOrderReturnRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
+
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
+
     public function rules(): array
     {
         return [
+            'customer_id' => 'required|integer|exists:customers,id',
             'product_list' => 'required|array',
             'product_list.*.product_id' => 'required|integer|exists:products,id',
             'product_list.*.polka_id' => 'required|integer|exists:product_stocks,id',
