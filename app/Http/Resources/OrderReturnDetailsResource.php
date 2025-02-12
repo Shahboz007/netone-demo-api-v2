d16f63500ad725caaca1e9a94f5666982dab900e
shahboz007 <olmasovshahboz007@gmail.com>
2025-02-12 17:05:23 +0500

    change of OrderReturnDetails Resource

--- app/Http/Resources/OrderReturnDetailsResource.php
+++ app/Http/Resources/OrderReturnDetailsResource.php
@@ -12,12 +12,18 @@ class OrderReturnDetailsResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'product' => ProductResource::make($this->product),
+            'product_stock' => [
+                'id' => $this->productStock->id,
+                'name' => $this->productStock->name,
+                'amount_type_id' => $this->productStock->amount_type_id,
+            ],
             'amount_type' => $this->amountType,
             'amount' => (float)$this->amount,
-            'cost_price' => (float) $this->cost_price,
+            'cost_price' => auth()->user()->isAdmin() ? (float) $this->cost_price : 0,
             'sale_price' => (float) $this->sale_price,
             'sum_sale_price' => (float) $this->sum_sale_price,
-            'sum_cost_price' => (float) $this->sum_cost_price,
+            'sum_cost_price' => auth()->user()->isAdmin() ?  (float) $this->sum_cost_price : 0,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
