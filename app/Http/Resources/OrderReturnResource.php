9eb46032f58157e93927b84d83e387d48bd62bd3
shahboz007 <olmasovshahboz007@gmail.com>
2025-02-12 16:45:13 +0500

    change of OrderReturn resources

--- app/Http/Resources/OrderReturnResource.php
+++ app/Http/Resources/OrderReturnResource.php
@@ -7,19 +7,14 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class OrderReturnResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(Request $request): array
     {
         return [
             'id' => $this->id,
             'user' => UserResource::make($this->user),
             'customer' => CustomerResource::make($this->customer),
+            'total_cost_price' => auth()->user()->isAdmin() ? (float) $this->total_cost_price : 0,
             'total_sale_price' => (float) $this->total_sale_price,
-            'total_cost_price' => (float) $this->total_cost_price,
             'comment' => $this->comment,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
