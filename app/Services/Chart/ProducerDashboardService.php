edb70c1095e77e83c5dd3ecfb48d965b9088fe4e
shahboz007 <olmasovshahboz007@gmail.com>
2025-02-20 16:48:08 +0500

    done ProducerDashboardService

--- app/Services/Chart/ProducerDashboardService.php
+++ app/Services/Chart/ProducerDashboardService.php
@@ -10,16 +10,18 @@ class ProducerDashboardService
 {
     public function get()
     {
-        $status = Status::where('code', 'orderSubmitted');
-
-        $todaySales = $this->todaySalesWithDiff();
+        $todaySales = $this->todaySales();
+        $todayExpense = $this->todayExpense();
+        $todayIncome = $this->todayIncome($todaySales, $todayExpense);
 
         return [
             "sales" => $todaySales,
+            "expense" => $todayExpense,
+            "income" => $todayIncome,
         ];
     }
 
-    private function todaySalesWithDiff()
+    private function todaySales()
     {
         $todayAmount = (float) DB::table("completed_orders")
             ->selectRaw('SUM(total_sale_price) as total_sale_price')
@@ -40,9 +42,59 @@ class ProducerDashboardService
 
 
         return [
-            "amount" => $todayAmount,
+            'yesterday_amount' => $yesterdayAmount,
+            "today_amount" => $todayAmount,
             "diff_amount" => $diffAmount,
             "diff_percent" => $diffPercent,
+        ];
+    }
+
+    private function todayExpense()
+    {
+        $todayExpense = DB::table('payments')
+            ->join('payment_wallet', 'payments.id', '=', 'payment_wallet.payment_id')
+            ->select(
+                DB::raw('COUNT(payments.id) as total_count'),
+                DB::raw('SUM(payment_wallet.sum_price) as total_amount'),
+            )
+            ->where('payments.paymentable_type', 'App\Models\Expense')
+            ->whereDate('payments.created_at', today())
+            ->first();
+
+        $yesterdayExpense = DB::table('payments')
+            ->join('payment_wallet', 'payments.id', '=', 'payment_wallet.payment_id')
+            ->select(
+                DB::raw('COUNT(payments.id) as total_count'),
+                DB::raw('SUM(payment_wallet.sum_price) as total_amount'),
+            )
+            ->where('payments.paymentable_type', 'App\Models\Expense')
+            ->whereDate('payments.created_at', Carbon::yesterday())
+            ->first();
+
+        // Amounts
+        $todayAmount = (float) $todayExpense->total_amount;
+        $yesterdayAmount = (float) $yesterdayExpense->total_amount;
+
+        $diffAmount = $todayAmount - $yesterdayAmount;
+        $diffPercent = 100;
+        if ($yesterdayAmount != 0) {
+            $diffPercent = ($todayAmount ?? 1) * 100 / $yesterdayAmount - 100;
+        }
+
+        return [
+            "yesterday_amount" => $yesterdayAmount,
+            "yesterday_count" => $yesterdayExpense->total_count,
+            "today_amount" => $todayAmount,
+            "today_count" => $todayExpense->total_count,
+            "diff_amount" => $diffAmount,
+            "diff_percent" => $diffPercent
+        ];
+    }
+
+    private function todayIncome($todaySales, $todayExpense)
+    {
+        return [
+            "amount" => $todaySales['today_amount'] - $todayExpense['today_amount']
         ];
     }
 }
