0a5577fe3602770f63a05c3fca3a555cd8104f23
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2025-03-01 15:43:47 +0500

    receive product refactored

--- app/Services/Receive/ReceiveProductService.php
+++ app/Services/Receive/ReceiveProductService.php
@@ -2,8 +2,15 @@
 
 namespace App\Services\Receive;
 
+use App\Exceptions\ServerErrorException;
+use App\Models\Product;
+use App\Models\ProductStock;
 use App\Models\ReceiveProduct;
+use App\Models\Supplier;
+use App\Services\Status\StatusService;
 use App\Services\Utils\DateFormatter;
+use Carbon\Exceptions\InvalidDateException;
+use Illuminate\Support\Facades\DB;
 
 class ReceiveProductService
 {
@@ -41,7 +48,132 @@ class ReceiveProductService
 
     public function create(array $data)
     {
+        // Data
+        $supplierId = $data['supplier_id'];
+        $dateReceived = $data['date_received'];
+        $reqProductList = $data['product_list'];
+        $comment = $data['comment'] ?? null;
 
+         // Req Product Plucked List
+        $reqPolkaArr = array_column($reqProductList, 'polka_id');
+
+        // Stock
+        $stockList = ProductStock::whereIn('id', $reqPolkaArr)->get();
+        $pluckStockAmount = $stockList->pluck('amount', 'id')->toArray();
+        $pluckStockName = $stockList->pluck('name', 'id');
+        $pluckStockProductId = $stockList->pluck('product_id', 'id');
+
+        if ($stockList->isEmpty()) {
+            throw new InvalidDateException('Mahsulotlar uchun zaxira polka ochilmagan. Admin zaxira polka yaratishi kerak');
+        }
+
+        // Products
+        $products = Product::whereIn('id', array_column($reqProductList, 'product_id'))->get();
+        $pluckProductsName = $products->pluck('name', 'id')->toArray();
+        $pluckProductsPriceAmountType = $products->pluck('price_amount_type_id', 'id')->toArray();
+
+        // Stock Items
+        foreach ($reqProductList as $item) {
+            if (isset($pluckStockProductId[$item['polka_id']])) {
+                // Check match
+                if ($pluckStockProductId[$item['polka_id']] !== $item['product_id']) {
+
+
+                    $stockName = $pluckStockName[$item['polka_id']];
+                    $productName = $pluckProductsName[$item['product_id']];
+
+                    throw new InvalidDateException("`$productName` mahsulot uchun `$stockName` nomli polka tegishli emas. Polkani to'g'ri tanlang");
+                }
+
+                if (!isset($pluckStockAmount[$item['polka_id']])) {
+                    $productName = $pluckProductsName[$item['product_id']];
+                    throw new InvalidDateException("`$productName` mahsulot uchun zaxira polka ochilmagan. Adminka zaxira polka yaratishi kerak");
+                }
+
+                // Check match stock product_id with request product_id
+                if ($pluckStockProductId[$item['polka_id']] !== $item['product_id']) {
+                    $stockName = $pluckStockName[$item['polka_id']];
+                    $productName = $pluckProductsName[$item['product_id']];
+
+                    throw new InvalidDateException("`$stockName` polka `$productName` mahsulotga tegishli emas!");
+                }
+            } else {
+                throw new InvalidDateException("Polka topilmadi");
+            }
+        }
+
+
+        // Supplier
+        $supplier = Supplier::findOrFail($supplierId);
+
+        // Status Receive Debt
+        $statusReceiveDebt = StatusService::findByCode('receiveProductDebt');
+
+        DB::beginTransaction();
+
+        try {
+            // New Receive
+            $newReceive = ReceiveProduct::create([
+                'user_id' => auth()->id(),
+                'supplier_id' => $supplier->id,
+                'status_id' => $statusReceiveDebt->id,
+                'date_received' => $dateReceived,
+                'total_price' => 0,
+                'comment' => $comment,
+            ]);
+            $totalPrice = 0;
+
+            // Attach Details
+            $productList = [];
+
+            foreach ($reqProductList as $item) {
+                $sum = $item['amount'] * $item['price'];
+
+                $productList[] = [
+                    'receive_product_id' => $newReceive->id,
+                    'product_id' => $item['product_id'],
+                    'amount' => $item['amount'],
+                    'price' => $item['price'],
+
+                    'amount_type_id' => $pluckProductsPriceAmountType[$item['product_id']],
+                    'status_id' => $statusReceiveDebt->id,
+                    'sum_price' => $sum,
+                ];
+
+                $totalPrice += $sum;
+
+                // Change Product Receive Price
+                $productItem = Product::where('id', $item['product_id'])->firstOrFail();
+                $productItem->receive_price = $item['price'];
+                $productItem->save();
+
+                // Change Stock Amount
+                $stockItem = ProductStock::where('id', $item['polka_id'])
+                    ->where('product_id', $item['product_id'])
+                    ->firstOrFail();
+
+                $stockItem->increment('amount', $item['amount']);
+            }
+
+            $newReceive->receiveProductDetails()->createMany($productList);
+
+            $newReceive->total_price = $totalPrice;
+            $newReceive->save();
+
+            // Change of Supplier's balance
+            $supplier->increment('balance', $totalPrice);
+            DB::commit();
+
+            $formatVal = number_format($totalPrice, 2, '.', ',');
+
+            return [
+                'message' => "Yuk muvaffaqiyatli qabul qilindi. Jami $formatVal uzs.",
+                'data' => $newReceive
+            ];
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw new ServerErrorException($e->getMessage(), $e->getCode(), $e);
+        }
     }
 
     public function findOne(int $id): array
