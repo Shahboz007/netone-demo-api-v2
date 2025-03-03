41b939167f6f2ca023838281c150c9d768dbf5e2
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2025-03-03 18:39:58 +0500

    change of PaymentExpense create

--- app/Http/Controllers/Finance/PaymentExpenseController.php
+++ app/Http/Controllers/Finance/PaymentExpenseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Finance;
 
+use App\Exceptions\ServerErrorException;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\QueryParameterRequest;
 use App\Http\Requests\StorePaymentExpenseRequest;
@@ -32,82 +33,25 @@ class PaymentExpenseController extends Controller
         ]);
     }
 
-    public function store(StorePaymentExpenseRequest $request): ?JsonResponse
+    /**
+     * @throws ServerErrorException
+     */
+    public function store(StorePaymentExpenseRequest $request): JsonResponse
     {
-        $reqAmount = $request->validated('amount');
 
-        // Validation User Wallet
-        $userWallet = auth()->user()->wallets()->wherePivot('wallet_id', $request->validated('wallet_id'))->firstOrFail();
+        $result = $this->paymentExpenseService->create($request->validated());
 
-        if ($userWallet->pivot->amount < $reqAmount) {
-            abort(422, "`$userWallet->name` bu hisobingizda mablag' yetarli emas! Hisobingizni tekshiring");
-        }
-
-        // Expense
-        $expense = Expense::findOrFail($request->validated('expense_id'));
-
-        // Status Payment Expense
-        $statusPaymentExpense = Status::where('code', 'paymentExpense')->firstOrFail();
-
-        DB::beginTransaction();
-
-        try {
-            // New Payment For Expense
-            $payment = new Payment([
-                'user_id' => auth()->id(),
-                'status_id' => $statusPaymentExpense->id,
-                'comment' => $request->validated('comment')
-            ]);
-            $expense->payments()->save($payment);
-
-            // Attach Amount To Wallet
-            $payment->wallets()->attach($request->validated('wallet_id'), [
-                'amount' => $reqAmount,
-                'rate_amount' => $request->validated('rate_amount'),
-                'sum_price' => $reqAmount * $request->validated('rate_amount'),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
-
-            // Decrement from User Wallet
-            DB::table('user_wallet')
-                ->where('user_id', auth()->id())
-                ->where('wallet_id', $request->validated('wallet_id'))
-                ->update(['amount' => DB::raw("amount - {$reqAmount}")]);
-
-            // Finish
-            DB::commit();
-
-            // Amount Currency
-            $currency = $userWallet->currency->symbol;
-
-            $formatNum = number_format($reqAmount, 2, '.', ',');
-
-            return response()->json([
-                'message' => "`$expense->name` xarajat uchun $formatNum $currency  muvaffaqiyatli o'tkazildi!",
-            ], 201);
-
-        } catch (\Exception $e) {
-            DB::rollBack();
-            return $this->serverError($e);
-        }
-
+        return response()->json([
+            'message' => $result['message'],
+        ], 201);
     }
 
     public function show(string $id): JsonResponse
     {
-        $query = Payment::with('paymentable', 'user', 'wallets', 'status')
-            ->where('paymentable_type', 'App\Models\Expense')
-            ->orderBy('created_at', 'desc');
-
-        if (!auth()->user()->isAdmin()) {
-            $query->where('user_id', auth()->id());
-        }
-
-        $data = $query->findOrFail($id);
+        $result = $this->paymentExpenseService->findOne($id);
 
         return response()->json([
-            'data' => PaymentExpenseResource::make($data),
+            'data' => PaymentExpenseResource::make($result['data']),
         ]);
     }
 }
--- app/Services/Payment/PaymentExpenseService.php
+++ app/Services/Payment/PaymentExpenseService.php
@@ -2,7 +2,12 @@
 
 namespace App\Services\Payment;
 
+use App\Exceptions\InvalidDataException;
+use App\Exceptions\ServerErrorException;
+use App\Models\Expense;
 use App\Models\Payment;
+use App\Models\UserWallet;
+use App\Services\Status\StatusService;
 use App\Services\Utils\DateFormatter;
 use Illuminate\Support\Facades\DB;
 
@@ -10,7 +15,7 @@ class PaymentExpenseService
 {
     public function findAll(array $data): array
     {
-        $startDate = DateFormatter::format($data['startDate'], 'start');
+        $startDate = DateFormatter::format($data['startDate']);
         $endDate = DateFormatter::format($data['endDate'], 'end');
         $expenseId = $data['expense_id'] ?? null;
 
@@ -35,14 +40,89 @@ class PaymentExpenseService
         ];
     }
 
-    public function findOne()
-    {
 
+    /**
+     * @throws ServerErrorException
+     * @throws InvalidDataException
+     */
+    public function create(array $data): array
+    {
+        $expenseId = $data['expense_id'];
+        $walletId = $data['wallet_id'];
+        $amount = $data['amount'];
+        $rateAmount = $data['rate_amount'];
+        $comment = $data['comment'] ?? null;
+
+        // Validation User Wallet
+        $userWallet = UserWallet::with('wallet.currency')->where('user_id', auth()->id())
+            ->where('wallet_id', $walletId)
+            ->firstOrFail();
+
+        if ($userWallet->amount < $amount) {
+            throw new InvalidDataException("`$userWallet->name` bu hisobingizda mablag' yetarli emas! Hisobingizni tekshiring");
+        }
+
+        // Expense
+        $expense = Expense::findOrFail($expenseId);
+
+        // Status Payment Expense
+        $statusPaymentExpense = StatusService::findByCode('paymentExpense');
+
+        DB::beginTransaction();
+
+        try {
+            // New Payment For Expense
+            $payment = new Payment([
+                'user_id' => auth()->id(),
+                'status_id' => $statusPaymentExpense->id,
+                'comment' => $comment
+            ]);
+            $expense->payments()->save($payment);
+
+            // Attach Amount To Wallet
+            $payment->wallets()->attach($walletId, [
+                'amount' => $amount,
+                'rate_amount' => $rateAmount,
+                'sum_price' => $amount * $rateAmount,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+
+            // Decrement from User Wallet
+            $userWallet->decrement('amount', $amount);
+
+            // Finish
+            DB::commit();
+
+            // Amount Currency
+            $currency = $userWallet->wallet->currency->symbol;
+
+            $formatNum = number_format($amount, 2, '.', ',');
+
+            return [
+                'message' => "`$expense->name` xarajat uchun $formatNum $currency  muvaffaqiyatli o'tkazildi!",
+            ];
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw new ServerErrorException($e->getMessage(), $e->getCode(), $e);
+        }
     }
 
-    public function create(array $data)
+    public function findOne(int $expenseId): array
     {
+        $query = Payment::with('paymentable', 'user', 'wallets', 'status')
+            ->where('paymentable_type', 'App\Models\Expense')
+            ->orderBy('created_at', 'desc');
 
+        if (!auth()->user()->isAdmin()) {
+            $query->where('user_id', auth()->id());
+        }
+
+        $data = $query->findOrFail($expenseId);
+
+        return [
+            'data' => $data,
+        ];
     }
 
     private function getTotals(string|null $expenseId, array $date): array
