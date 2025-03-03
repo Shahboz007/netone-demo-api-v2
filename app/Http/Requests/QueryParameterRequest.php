d0c47938a47e2aca26bd4277061c6e52b15f5a3c
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2025-03-03 10:19:06 +0500

    add QueryParameterRequest

---
+++ app/Http/Requests/QueryParameterRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class QueryParameterRequest extends FormRequest
+{
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+
+    public function rules(): array
+    {
+        return [
+            'startDate' => 'required|date|date_format:d-m-Y|before_or_equal:endDate',
+            'endDate' => 'required|date|date_format:d-m-Y|after_or_equal:startDate',
+            'supplier_id' => 'nullable|integer|exists:suppliers,id',
+        ];
+    }
+}
