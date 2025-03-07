493ee9ffa3868f825b689db608b9b245253eae04
shahboz007 <olmasovshahboz007@gmail.com>
2025-03-07 15:29:14 +0500

    done store CustomerRentalProperty

---
+++ app/Services/RentalProperty/CustomerRentalPropertyService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services\RentalProperty;
+
+class CustomerRentalProperty
+{
+    public function findAll()
+    {
+
+    }
+
+    public function create(array $data)
+    {
+        // Data
+        $req = $data[''];
+    }
+    public function findOne($id)
+    {
+
+    }
+}
