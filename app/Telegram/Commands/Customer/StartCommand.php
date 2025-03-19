654073cc3f789d85cf8c6f2e8b421b846e825cf4
shahboz007 <olmasovshahboz007@gmail.com>
2025-03-19 12:04:05 +0500

    done StartCommand with Customer Logic

--- app/Telegram/Commands/Customer/StartCommand.php
+++ app/Telegram/Commands/Customer/StartCommand.php
@@ -17,12 +17,12 @@ class StartCommand implements TelegramCommandInterface
     $customer = Customer::where('telegram', $chatId)->first();
 
     // Body
-    $body = "<b>Assalomu alaykum</b>\n\nNetOnega xush kelibsiz! Siz telegram botimiz yordamida berilgan buyurtmalar, qarzdorlik, to'lovlar va aktsverka hisobotlarini ko'rib borishingiz mumkin\n\n✅ <code>$chatId</code> - <i>bu id raqam yordamida bizning xodimlarimiz sizni tizimga kiritishadi va siz telegram botimizni ishlatishingiz mumkin</i>";
+    $body = "<b>Assalomu alaykum</b>\n\nNetOnega xush kelibsiz! Siz telegram botimiz yordamida berilgan buyurtmalar, qarzdorlik, to'lovlar va aktsverka hisobotlarini ko'rib borishingiz mumkin\n\n✅ <code>$chatId</code> - <i>bu id raqam yordamida bizning xodimlarimiz sizni tizimga kiritishadi va siz telegram botimizni ishlatishingiz mumkin</i>\n\nSizning holatingiz:\n";
 
     if ($customer) {
-      $chat->html($body)->replyKeyboard(HomeReplyKeyboard::make())->send();
+      $chat->html($body."🟢 faol")->replyKeyboard(HomeReplyKeyboard::make())->send();
     } else {
-      $chat->html($body)->removeReplyKeyboard()->send();
+      $chat->html($body."🔴 faolsiz\n<i>Bot imkoniyatidan to'liq foydalana olmaysiz!</i>")->removeReplyKeyboard()->send();
     }
   }
 }
