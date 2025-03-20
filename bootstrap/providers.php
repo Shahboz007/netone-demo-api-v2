bb8182fc6d6b4cea7f21eb139bcd69bd80e207b9
shahboz007 <olmasovshahboz007@gmail.com>
2025-03-20 15:52:55 +0500

    Add CustomerServiceProvider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
+    App\Providers\CustomerServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
 ];
