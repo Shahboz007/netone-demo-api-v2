eeb28b6eb1a5127af2070e63597056837f2040e2
shahboz007 <olmasovshahboz007@gmail.com>
2025-03-20 12:16:32 +0500

    refactored

--- app/Telegram/Commands/Customer/OrdersCommand.php
+++ app/Telegram/Commands/Customer/OrdersCommand.php
@@ -3,11 +3,7 @@
 namespace App\Telegram\Commands\Customer;
 
 use App\Contracts\TelegramCommandInterface;
-use App\Services\Order\OrderTelegramService;
 use App\Telegram\Actions\Customer\OrderPaginationAction;
-use App\Telegram\Enums\CustomerCommandEnum;
-use App\Telegram\Keyboards\Customer\OrderPaginationKeyboard;
-use App\Telegram\MessageBody\Customer\OrderMessageBody;
 use DefStudio\Telegraph\Models\TelegraphChat;
 
 class OrdersCommand implements TelegramCommandInterface
