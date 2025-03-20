e08bb5436cb8b11776795774f0013f4ee5bf24c2
shahboz007 <olmasovshahboz007@gmail.com>
2025-03-20 15:41:31 +0500

    add order events

--- app/Services/Order/OrderTelegramService.php
+++ app/Services/Order/OrderTelegramService.php
@@ -6,6 +6,7 @@ use App\Models\Order;
 
 class OrderTelegramService
 {
+  // Get Order Date By Paginate
   public function paginate($customerId, int $perPage = 5, ?int $page = null)
   {
     return Order::with([
@@ -16,4 +17,15 @@ class OrderTelegramService
       ->where('customer_id', $customerId)
       ->paginate($perPage, ['*'], 'page', $page);
   }
+
+  // New Order Msg
+  public function newOrderMsg() {}
+  // Process Order Msg
+  public function processOrderMsg() {}
+  // Cancel Order Msg
+  public function cancelOrderMsg() {}
+  // Completed Order Msg
+  public function CompletedOrderMsg() {}
+  // Submitted Order Msg
+  public function SubmittedOrderMsg() {}
 }
