683440fbd2f257106359e34dbf13dbebbb579ad3
shahboz007 <olmasovshahboz007@gmail.com>
2025-03-20 13:57:23 +0500

    done settings

--- app/Telegram/Commands/Customer/SettingsCommand.php
+++ app/Telegram/Commands/Customer/SettingsCommand.php
@@ -3,13 +3,30 @@
 namespace App\Telegram\Commands\Customer;
 
 use App\Contracts\TelegramCommandInterface;
+use App\Models\Customer;
+use App\Services\Utils\PhoneNumber;
 use DefStudio\Telegraph\Models\TelegraphChat;
 
 class SettingsCommand implements TelegramCommandInterface
 {
   public static function handle(TelegraphChat $chat): void
   {
-    $chat->markdownV2("**Sozlamalar**")
+    $message = "<b>Sozlamalar</>\n\n";
+
+    // Customer
+    $customer = Customer::where('telegram', $chat->chat_id)->firstOrFail();
+
+    // Name
+    $message .= "👤Ism:   {$customer->last_name[0]}.$customer->first_name\n";
+    $val = PhoneNumber::format($customer->phone);
+
+    // Phone
+    $message .= "📞Telefon:   {$val}\n";
+
+    // Status
+    $message .= "🟢Holati:    Faol\n\n";
+
+    $chat->html($message)
       ->send();
   }
 }
