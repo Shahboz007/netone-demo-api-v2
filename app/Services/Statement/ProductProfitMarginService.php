06d20a53332f222e802be99b91a84c570706d380
shahboz007 <olmasovshahboz007@gmail.com>
2025-03-25 10:28:42 +0500

    change of product profit margin service

--- app/Services/Statement/ProductProfitMarginService.php
+++ app/Services/Statement/ProductProfitMarginService.php
@@ -18,8 +18,7 @@ class ProductProfitMarginService
     $query = DB::table('products')
       ->leftJoin('order_details', 'products.id', '=', 'order_details.product_id')
       ->select(
-        'products.id as product_id',
-        'products.name as product_name',
+        'products.*',
         DB::raw('COALESCE(SUM(order_details.sum_sale_price - order_details.sum_cost_price), 0) as total_profit')
       )
       ->whereBetween('order_details.created_at', [$startDate, $endDate])
@@ -30,6 +29,10 @@ class ProductProfitMarginService
 
 
     foreach ($data as $item) {
+      $item->cost_price = (float) $item->cost_price;
+      $item->receive_price = (float) $item->receive_price;
+      $item->sale_price = (float) $item->sale_price;
+      
       $item->total_profit = (float) $item->total_profit;
     }
 
