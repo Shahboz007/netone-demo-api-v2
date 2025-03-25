03b90bd1dfe7401cf36ef3825e35d7164b5b1720
shahboz007 <olmasovshahboz007@gmail.com>
2025-03-25 17:29:03 +0500

    change of AuthController login logic

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -7,7 +7,9 @@ use App\Http\Requests\AuthLoginRequest;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Session\CookieSessionHandler;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
@@ -31,14 +33,20 @@ class AuthController extends Controller
         DB::table('personal_access_tokens')->where('tokenable_id', $user->id)->delete();
 
         // Create new Token
-        $token = $user->createToken('auth_token')->plainTextToken;
+        $accessToken = $user->createToken('auth_token')->plainTextToken;
+        // $refreshToken = 
 
         // Response
         return response()->json([
             "message" => "Xush kelibsiz!",
-            "token" => $token,
-            "data" => $user
-        ], 200, ['token' => $token]);
+            "access_token" => $accessToken,
+            "data" => [
+                'user_id' => $user,
+                'access_token' => $accessToken,
+            ]
+        ], 200)
+            ->cookie('access_token', $accessToken, 15, '/', null, true, true)
+            ->cookie('refresh_token', "my_refresh", 60 * 24, '/', null, false, true); // 24h
     }
 
     public function logout(Request $request): JsonResponse
